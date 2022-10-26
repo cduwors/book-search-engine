8af831e3dc96f39087344904dccb0a8eca79d9e4
cduwors <christinaduwors@gmail.com>
2022-10-26 15:52:29 -0400

    back end complete

--- server/server.js
+++ server/server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
-
+const { authMiddleware } = require("./utils/auth");
 const path = require("path");
 
-const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
-
-const routes = require("./routes");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+	context: authMiddleware,
 });
 
 const app = express();
@@ -27,9 +26,6 @@ if (process.env.NODE_ENV === "production") {
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
-
-app.use(routes);
-
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
